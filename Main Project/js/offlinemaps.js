ad6789d77fa759505cff35edabb8499a1c6e6439
Dylan Maryk <dylan@dylanmaryk.com>
2015-11-08 20:49:16 +0000

    Create map using Leaflet JavaScript library, download map tiles from OpenStreetMap when needed and add additional information on top from .osm file. Displaying additional information works offline, displaying map tiles does not yet.

--- Main Project/js/offlinemaps.js
+++ Main Project/js/offlinemaps.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 	$.get("london.osm", function(data) {
-		dataParsed = $.parseXML(data);
-		console.log(osmtogeojson(dataParsed));
+    	var map = L.map("map");
+    	map.setView(new L.LatLng(51.505, -0.09), 9);
+
+		var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+		var osm = new L.TileLayer(osmUrl);
+		osm.addTo(map);
+
+		var dataParsed = $.parseXML(data);
+		var dataGeoJson = osmtogeojson(dataParsed);
+		L.geoJson(dataGeoJson).addTo(map);
 	});
 });
--- Main Project/updatedata.sh
+++ Main Project/updatedata.sh
@@ -1,2 +1,8 @@
+wget -O css/leaflet.css "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css"
+wget -O js/leaflet.js "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"
+wget -O js/images/marker-icon.png "http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-icon.png"
+wget -O js/images/marker-shadow.png "http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-shadow.png"
+
 wget -O js/osmtogeojson.js "https://raw.githubusercontent.com/tyrasd/osmtogeojson/gh-pages/osmtogeojson.js"
+
 wget -O london.osm "http://api06.dev.openstreetmap.org/api/0.6/map?bbox=-0.221272,51.4801,-0.071754,51.533309"
