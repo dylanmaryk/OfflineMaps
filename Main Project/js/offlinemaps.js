57002c244462b3e6248a9c1a67b449477c4ae888
Dylan Maryk <dylan@dylanmaryk.com>
2015-11-19 19:12:25 +0000

    Implement getting image tiles from database using current positions and zoom level of map as keys.

--- Main Project/js/offlinemaps.js
+++ Main Project/js/offlinemaps.js
@@ -35,11 +35,11 @@ $(document).ready(function() {
 	map.setView(new L.LatLng(51.505, -0.09), 12);
 
 	var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-	var osm = new CustomTileLayer(osmUrl);
-	osm.on("tileloadstart", function(event) {
+	var osmLayer = new CustomTileLayer(osmUrl);
+	osmLayer.on("tileloadstart", function(event) {
 		event.tile.crossOrigin = "Anonymous";
 	});
-	osm.on("tileload", function(event) {
+	osmLayer.on("tileload", function(event) {
 		var tileImageString = getBase64Image(event.tile);
 		var tileImagePoint = event.tile.point;
 
@@ -49,7 +49,23 @@ $(document).ready(function() {
 			tilesToStore.push({image: tileImageString, point: tileImagePoint});
 		}
 	});
-	osm.addTo(map);
+	osmLayer.on("tileerror", function(event) {
+		if (db) {
+			var tile = event.tile;
+			var tileImagePointString = getPointString(tile.point);
+			var request = getObjectStore().get(tileImagePointString);
+			request.onsuccess = function(event) {
+				var tileImageString = request.result;
+
+				if (tileImageString) {
+					tile.src = "data:image/png;base64," + tileImageString;
+				} else {
+					console.log("No tile image stored for point " + tileImagePointString + ".");
+				}
+			};
+		}
+	});
+	osmLayer.addTo(map);
 
 	$.get("london.osm", function(data) {
 		var dataParsed = $.parseXML(data);
@@ -59,11 +75,19 @@ $(document).ready(function() {
 });
 
 function storeTileImage(tileImageString, tileImagePoint) {
+	getObjectStore().add(tileImageString, getPointString(tileImagePoint));
+}
+
+function getObjectStore() {
+	return db.transaction("tile", "readwrite").objectStore("tile");
+}
+
+function getPointString(tileImagePoint) {
 	var x = tileImagePoint.x;
 	var y = tileImagePoint.y;
 	var z = tileImagePoint.z;
-	var objectStore = db.transaction("tile", "readwrite").objectStore("tile");
-	objectStore.add(tileImageString, x + "," + y + "," + z);
+
+	return x + "," + y + "," + z;
 }
 
 // From http://stackoverflow.com/a/19183658
@@ -71,21 +95,26 @@ function getBase64Image(img) {
 	var canvas = document.createElement("canvas");
 	canvas.width = img.width;
 	canvas.height = img.height;
-
 	var ctx = canvas.getContext("2d");
 	ctx.drawImage(img, 0, 0);
-
 	var dataURL = canvas.toDataURL("image/png");
 
 	return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
-// Required to ensure we can set the tile's "crossOrigin" parameter before its "src" parameter is set
 var CustomTileLayer = L.TileLayer.extend({
+	// Required to ensure we can set the tile's "crossOrigin" parameter before its "src" parameter is set
 	_loadTile: function(t, e) {
 		t._layer = this, t.onload = this._tileOnLoad, t.onerror = this._tileOnError, this._adjustTilePoint(e), t.point = e, this.fire("tileloadstart", {
 			tile: t,
 			url: t.src
 		}), t.src = this.getTileUrl(e)
+	},
+	_tileOnError: function() {
+		var t = this._layer;
+		t.fire("tileerror", {
+			tile: this,
+			url: this.src
+		});
 	}
 });
