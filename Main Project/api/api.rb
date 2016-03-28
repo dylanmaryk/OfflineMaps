7e88c23e3db2fcd1482e43667e58c0eaa76bd1f3
Dylan Maryk <dylan@dylanmaryk.com>
2016-03-28 16:18:45 +0100

    Disable API logging for privacy reasons.

--- Main Project/api/api.rb
+++ Main Project/api/api.rb
@@ -4,6 +4,7 @@ require 'sinatra/cross_origin'
  
 set :port, 8110
 set :environment, :production
+set :logging, false
 
 configure do
 	enable :cross_origin
@@ -14,5 +15,3 @@ get '/name' do
 	res = conn.exec_params('SELECT * FROM geoname ORDER BY ST_Distance(coord, ST_SetSRID(ST_MakePoint($1, $2), 4326)) ASC LIMIT 1', [params[:lat], params[:long]])
 	res[0]['name']
 end
-
-# 48.7000 44.5167
