5175f730ea9c394dcdc01c1c5552220f980750e6
Dylan Maryk <dylan@dylanmaryk.com>
2016-03-29 01:58:58 +0100

    Remove redundant JS file references.

--- Main Project/updatedata.sh
+++ Main Project/updatedata.sh
@@ -3,8 +3,6 @@ wget -O js/leaflet.js "http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"
 wget -O js/images/marker-icon.png "http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-icon.png"
 wget -O js/images/marker-shadow.png "http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-shadow.png"
 
-wget -O js/osmtogeojson.js "https://raw.githubusercontent.com/tyrasd/osmtogeojson/gh-pages/osmtogeojson.js"
-
 wget -O img/download.png "https://raw.githubusercontent.com/google/material-design-icons/master/navigation/2x_web/ic_arrow_downward_black_18dp.png"
 
 wget -O cities1000.zip "http://download.geonames.org/export/dump/cities1000.zip"
